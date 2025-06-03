7c1b4a6a39f74286d940e665b18b1524223f254a
Dell <pabisasapkota116@gmail.com>
2025-06-03 12:46:07 +0300

    Class book has been created

--- Main Project/books.py
+++ Main Project/books.py
@@ -1,3 +1,35 @@
+import requests
+def fetch_book_details(title):
 
+    query = "+".join(title.split())
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if "items" in data:
+
+            book_data = data["items"][0]["volumeInfo"]
+            author = ", ".join(book_data.get("authors", ["Unknown Author"]))
+            summary = book_data.get("description", "No summary available.")
+            return {
+                "author": author,
+                "summary": summary
+            }
+        else:
+            print("No book found with that title.")
+    else:
+        print("Failed to connect to Google Books API.")
+
+    return {
+        "author": "Unknown Author",
+        "summary": "No summary available."
+    }
 class Book:
-        def fetch_book_details(title):
+        def __init__(self, title, author="Unknown", summary="No summary available."):
+                self.title = title
+                self.author = author
+                self.summary = summary
+
+        def __str__(self):
+                return f"{self.title} by {self.author} | {self.summary}"
